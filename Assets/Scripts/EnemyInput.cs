6eb8f7ad60d9b76da05e7487a4875c907df887aa
ytkimirti <yusuftaha9@gmail.com>
2019-04-27 09:07:12 +0300

    FİXED ALOT OMG

---
+++ Assets/Scripts/EnemyInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyInput : CharacterInput
+{
+    public Transform target;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (target)
+        {
+            movementInput = (target.position - transform.position);
+        }
+    }
+}
---
+++ Assets/Scripts/Spider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spider : Entity
+{
+    public float moveSpeed;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
